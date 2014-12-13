1539ea98df1acce8c1d62304d41f33f6a9038a2f
unknown <ArkadiuszN@ArkadiuszN-PC.insolutions.local>
2014-12-13 14:04:56 +0100

    Added fields - number of licences and program version

--- LicenseGenerator/ViewModels/LicenseViewModel.cs
+++ LicenseGenerator/ViewModels/LicenseViewModel.cs
@@ -14,5 +14,7 @@ namespace LicenseGenerator.ViewModels
         public string Nip { get; set; }
         public DateTime? Date { get; set; }
         public string Privileges { get; set; }
+        public int LicenseNumbers { get; set; }
+        public string ProgramVersion { get; set; }
     }
 }
