ed804fcaa6eda9b783a62759080383ea4c275233
Arkadiusz Neuman <mrmoro@o2.pl>
2015-01-03 14:54:35 +0100

    Added GeneratedLicense model

---
+++ LicenseGenerator/Models/GeneratedLicense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LicenseGenerator.Models
+{
+    public class GeneratedLicense
+    {
+        public string ProgramName { get; set; }
+        public string Company { get; set; }
+        public string NIP { get; set; }
+        public string Privileges { get; set; }
+        public DateTime LicenseTermDate { get; set; }
+        public string AddionalInformation { get; set; }
+        public int NumberOfStands { get; set; }
+        public string ProgramVersion { get; set; }
+        public string PartnerNIP { get; set; }
+    }
+}
